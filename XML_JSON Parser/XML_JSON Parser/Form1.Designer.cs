471a86162ffc0f1f29a762c2427f702858466723
abc873693 <abc873693@gmail.com>
2017-03-16 11:58:28 +0800

    first commit

---
+++ XML_JSON Parser/XML_JSON Parser/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace XML_JSON_Parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_get_data = new System.Windows.Forms.Button();
+            this.dataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.countyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.postalCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sectionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.englishDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countyBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_get_data
+            // 
+            this.button_get_data.Location = new System.Drawing.Point(619, 12);
+            this.button_get_data.Name = "button_get_data";
+            this.button_get_data.Size = new System.Drawing.Size(75, 23);
+            this.button_get_data.TabIndex = 0;
+            this.button_get_data.Text = "取得資料";
+            this.button_get_data.UseVisualStyleBackColor = true;
+            this.button_get_data.Click += new System.EventHandler(this.Button_get_data_Click);
+            // 
+            // dataBindingSource
+            // 
+            this.dataBindingSource.DataSource = typeof(XML_JSON_Parser.Data);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.postalCodeDataGridViewTextBoxColumn,
+            this.sectionDataGridViewTextBoxColumn,
+            this.englishDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.countyBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(601, 255);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // countyBindingSource
+            // 
+            this.countyBindingSource.DataSource = typeof(XML_JSON_Parser.County);
+            // 
+            // postalCodeDataGridViewTextBoxColumn
+            // 
+            this.postalCodeDataGridViewTextBoxColumn.DataPropertyName = "PostalCode";
+            this.postalCodeDataGridViewTextBoxColumn.HeaderText = "郵遞區號";
+            this.postalCodeDataGridViewTextBoxColumn.Name = "postalCodeDataGridViewTextBoxColumn";
+            this.postalCodeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // sectionDataGridViewTextBoxColumn
+            // 
+            this.sectionDataGridViewTextBoxColumn.DataPropertyName = "Section";
+            this.sectionDataGridViewTextBoxColumn.FillWeight = 200F;
+            this.sectionDataGridViewTextBoxColumn.HeaderText = "區域名稱";
+            this.sectionDataGridViewTextBoxColumn.Name = "sectionDataGridViewTextBoxColumn";
+            this.sectionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.sectionDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // englishDataGridViewTextBoxColumn
+            // 
+            this.englishDataGridViewTextBoxColumn.DataPropertyName = "English";
+            this.englishDataGridViewTextBoxColumn.FillWeight = 200F;
+            this.englishDataGridViewTextBoxColumn.HeaderText = "英文名稱";
+            this.englishDataGridViewTextBoxColumn.Name = "englishDataGridViewTextBoxColumn";
+            this.englishDataGridViewTextBoxColumn.ReadOnly = true;
+            this.englishDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 294);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_get_data);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countyBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_get_data;
+        private System.Windows.Forms.BindingSource dataBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource countyBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn postalCodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sectionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn englishDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ XML_JSON Parser/XML_JSON Parser/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XML_JSON_Parser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_get_data_Click(object sender, EventArgs e)
+        {
+            Data info;
+            XmlDocument doc = new XmlDocument();
+            string url = @"http://download.post.gov.tw/post/download/county_h_10603.xml";
+            var serializer = new XmlSerializer(typeof(Data));
+            using (var reader = XmlReader.Create(url))
+            {
+                info = (Data)serializer.Deserialize(reader);
+            }
+            foreach (County country in info.Countrys)
+            {
+                countyBindingSource.Add(country);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
