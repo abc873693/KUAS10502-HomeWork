5827c2fb5ebaf524e5465c3ebf0bfd319a359606
abc873693 <abc873693@gmail.com>
2017-04-13 21:14:24 +0800

    人工智慧第四次作業

--- 人工智慧/MF_fourth/MF_fourth/Form1.Designer.cs
+++ 人工智慧/MF_fourth/MF_fourth/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MF_third
+﻿namespace MF_fourth
 {
     partial class Form1
     {
@@ -28,30 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox_table_out = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_MF_out = new System.Windows.Forms.RichTextBox();
             this.button_confirm = new System.Windows.Forms.Button();
             this.textBox_X2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox_X1 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.richTextBox_MF_out = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_table_out = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // richTextBox_table_out
+            // richTextBox_MF_out
             // 
-            this.richTextBox_table_out.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.richTextBox_table_out.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox_table_out.Name = "richTextBox_table_out";
-            this.richTextBox_table_out.Size = new System.Drawing.Size(364, 263);
-            this.richTextBox_table_out.TabIndex = 0;
-            this.richTextBox_table_out.Text = "";
+            this.richTextBox_MF_out.Font = new System.Drawing.Font("新細明體", 9F);
+            this.richTextBox_MF_out.Location = new System.Drawing.Point(382, 124);
+            this.richTextBox_MF_out.Name = "richTextBox_MF_out";
+            this.richTextBox_MF_out.Size = new System.Drawing.Size(174, 151);
+            this.richTextBox_MF_out.TabIndex = 35;
+            this.richTextBox_MF_out.Text = "";
             // 
             // button_confirm
             // 
-            this.button_confirm.Location = new System.Drawing.Point(423, 95);
+            this.button_confirm.Location = new System.Drawing.Point(438, 95);
             this.button_confirm.Name = "button_confirm";
             this.button_confirm.Size = new System.Drawing.Size(75, 23);
-            this.button_confirm.TabIndex = 27;
+            this.button_confirm.TabIndex = 34;
             this.button_confirm.Text = "確定";
             this.button_confirm.UseVisualStyleBackColor = true;
             this.button_confirm.Click += new System.EventHandler(this.button_confirm_Click);
@@ -59,11 +59,11 @@
             // textBox_X2
             // 
             this.textBox_X2.Font = new System.Drawing.Font("新細明體", 14F);
-            this.textBox_X2.Location = new System.Drawing.Point(411, 59);
+            this.textBox_X2.Location = new System.Drawing.Point(423, 59);
             this.textBox_X2.Name = "textBox_X2";
             this.textBox_X2.Size = new System.Drawing.Size(100, 30);
-            this.textBox_X2.TabIndex = 26;
-            this.textBox_X2.Text = "0.7";
+            this.textBox_X2.TabIndex = 33;
+            this.textBox_X2.Text = "-0.25";
             this.textBox_X2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
@@ -72,18 +72,18 @@
             this.label1.Font = new System.Drawing.Font("Adobe Devanagari", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(382, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 26);
-            this.label1.TabIndex = 25;
-            this.label1.Text = "X2";
+            this.label1.Size = new System.Drawing.Size(41, 26);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "ê(k)";
             // 
             // textBox_X1
             // 
             this.textBox_X1.Font = new System.Drawing.Font("新細明體", 14F);
-            this.textBox_X1.Location = new System.Drawing.Point(411, 13);
+            this.textBox_X1.Location = new System.Drawing.Point(423, 13);
             this.textBox_X1.Name = "textBox_X1";
             this.textBox_X1.Size = new System.Drawing.Size(100, 30);
-            this.textBox_X1.TabIndex = 24;
-            this.textBox_X1.Text = "0.3";
+            this.textBox_X1.TabIndex = 31;
+            this.textBox_X1.Text = "0.25";
             this.textBox_X1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label8
@@ -92,24 +92,24 @@
             this.label8.Font = new System.Drawing.Font("Adobe Devanagari", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(382, 17);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(32, 26);
-            this.label8.TabIndex = 23;
-            this.label8.Text = "X1";
+            this.label8.Size = new System.Drawing.Size(41, 26);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "e(k)";
             // 
-            // richTextBox_MF_out
+            // richTextBox_table_out
             // 
-            this.richTextBox_MF_out.Font = new System.Drawing.Font("新細明體", 9F);
-            this.richTextBox_MF_out.Location = new System.Drawing.Point(382, 124);
-            this.richTextBox_MF_out.Name = "richTextBox_MF_out";
-            this.richTextBox_MF_out.Size = new System.Drawing.Size(174, 151);
-            this.richTextBox_MF_out.TabIndex = 28;
-            this.richTextBox_MF_out.Text = "";
+            this.richTextBox_table_out.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.richTextBox_table_out.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox_table_out.Name = "richTextBox_table_out";
+            this.richTextBox_table_out.Size = new System.Drawing.Size(364, 263);
+            this.richTextBox_table_out.TabIndex = 29;
+            this.richTextBox_table_out.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(568, 290);
+            this.ClientSize = new System.Drawing.Size(568, 286);
             this.Controls.Add(this.richTextBox_MF_out);
             this.Controls.Add(this.button_confirm);
             this.Controls.Add(this.textBox_X2);
@@ -127,13 +127,13 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox_table_out;
+        private System.Windows.Forms.RichTextBox richTextBox_MF_out;
         private System.Windows.Forms.Button button_confirm;
         private System.Windows.Forms.TextBox textBox_X2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox_X1;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.RichTextBox richTextBox_MF_out;
+        private System.Windows.Forms.RichTextBox richTextBox_table_out;
     }
 }
 
---
+++ 人工智慧/MF_fourth/MF_fourth/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MF_fourth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public String[,] table ={ 
+        { "NB", "NB", "NB", "NS", "PB" } ,
+        { "NB", "NS", "NS", "ZZ", "PB" } ,
+        { "NB", "NS", "ZZ", "PS", "PB" } ,
+        { "NB", "ZZ", "PN", "PS", "PB" } ,
+        { "ZB", "PS", "PB", "PB", "PB" } };
+        public Double[] Single_pole = { -10, -5, 0, 5, 10 };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int N = 5;
+
+            richTextBox_table_out.AppendText("    ");
+            for (int i = 0; i < N; i++)
+            {
+                richTextBox_table_out.AppendText(String.Format("{0,4}", RankCovert(i)));
+
+            }
+            richTextBox_table_out.AppendText("\n");
+            for (int i = 0; i < N; i++)
+            {
+                richTextBox_table_out.AppendText(String.Format("{0,4}", RankCovert(i)));
+                for (int j = 0; j < N; j++)
+                {
+                    richTextBox_table_out.AppendText(String.Format("{0,4}", table[i, j]));
+                }
+                richTextBox_table_out.AppendText("\n");
+            }
+        }
+
+        private int Rank_D(Double x)
+        {
+            Double[] num = { 0, 0.25, 0.5, 0.75, 1 };
+            Double[] distanse = { 0, 0.25, 0.5, 0.75, 1 };
+            for (int i = 0; i < 5; i++)
+            {
+                distanse[i] = Math.Abs(x - num[i]);
+            }
+            Double min = distanse[0];
+            int index = 0;
+            for (int i = 1; i < 5; i++)
+            {
+                if (distanse[i] < min)
+                {
+                    min = distanse[i];
+                    index = i;
+                }
+            }
+            return (index + 1);
+        }
+
+        private String Rank_NP(Double x)
+        {
+            Double[] num = { -10, -5, 0, 5, 10 };
+            Double[] distanse = new double[5];
+            for (int i = 0; i < 5; i++)
+            {
+                distanse[i] = Math.Abs(x - num[i]);
+            }
+            Double min = distanse[0];
+            int index = 0;
+            for (int i = 1; i < 5; i++)
+            {
+                if (distanse[i] < min)
+                {
+                    min = distanse[i];
+                    index = i;
+                }
+            }
+            switch (index)
+            {
+                case 0:
+                    return "NB";
+                case 1:
+                    return "NS";
+                case 2:
+                    return "ZZ";
+                case 3:
+                    return "PS";
+                case 4:
+                    return "PB";
+                default:
+                    return "PB";
+            }
+        }
+
+        private Double Rank_NP(String str)
+        {
+            switch (str)
+            {
+                case "NB":
+                    return Single_pole[0];
+                case "NS":
+                    return Single_pole[1];
+                case "ZZ":
+                    return Single_pole[2];
+                case "PS":
+                    return Single_pole[3];
+                case "PB":
+                    return Single_pole[4];
+                default:
+                    return Single_pole[0];
+            }
+        }
+
+        private String RankCovert(int i)
+        {
+            switch (i)
+            {
+                case 0: return "Vs";
+                case 1: return "S";
+                case 2: return "M";
+                case 3: return "L";
+                case 4: return "VL";
+                default: return "";
+            }
+        }
+
+        private void button_confirm_Click(object sender, EventArgs e)
+        {
+            if (CheckRange())
+            {
+                CalculateMF();
+            }
+            else
+            {
+                MessageBox.Show("數值範圍錯誤", "錯誤");
+            }
+        }
+
+        private void CalculateMF()
+        {
+            Double[] Y2 = { 1, 0, 1, 0, 1 };
+            Double[] Y1 = { 0, 1, 0, 1, 0 };
+            Double[] X = { -1, -0.25, 0, 0.25, 1 };
+            Double e = Convert.ToDouble(textBox_X1.Text);
+            Double e_dot = Convert.ToDouble(textBox_X2.Text);
+            int zone_e = -1, zone_e_dot = -1;
+            Double u1, u2, u3, u4;
+            for (int i = 0; i < X.Length - 1; i++)
+            {
+                if (e >= X[i] && e < X[i + 1]) zone_e = i;
+                if (e_dot >= X[i] && e_dot < X[i + 1]) zone_e_dot = i;
+            }
+            if (Y1[zone_e] > Y1[zone_e + 1])
+            {
+                u1 = (e - X[zone_e]) / 0.25;
+                u2 = 1 - u1;
+            }
+            else
+            {
+                u1 = 1 - (e - X[zone_e]) / 0.25;
+                u2 = 1 - u1;
+            }
+            if (Y2[zone_e_dot] > Y2[zone_e_dot + 1])
+            {
+                u3 = (e_dot - X[zone_e_dot]) / 0.25;
+                u4 = 1 - u3;
+            }
+            else
+            {
+                u3 = 1 - (e_dot - X[zone_e_dot]) / 0.25;
+                u4 = 1 - u3;
+            }
+            Double u13 = (u1 * u3), u23 = (u2 * u3), u14 = (u1 * u4), u24 = (u2 * u4);
+            richTextBox_MF_out.Text += "U1 =   " + u1.ToString("0.00") + "\n";
+            richTextBox_MF_out.Text += "U2 =   " + u2.ToString("0.00") + "\n";
+            richTextBox_MF_out.Text += "U3 =   " + u3.ToString("0.00") + "\n";
+            richTextBox_MF_out.Text += "U4 =   " + u4.ToString("0.00") + "\n";
+            richTextBox_MF_out.Text += "U13 =   " + u13.ToString("0.00") + "\n";
+            richTextBox_MF_out.Text += "U23 =   " + u23.ToString("0.00") + "\n";
+            richTextBox_MF_out.Text += "U14 =   " + u14.ToString("0.00") + "\n";
+            richTextBox_MF_out.Text += "U24 =   " + u24.ToString("0.00") + "\n";
+            Double U = (u13 * Rank_NP(table[zone_e_dot, zone_e]) + u23 * Rank_NP(table[zone_e_dot, zone_e]) + u14 * Rank_NP(table[zone_e_dot + 1, zone_e]) + u24 * Rank_NP(table[zone_e_dot + 1, zone_e + 1])) / (u13 + u23 + u14 + u24);
+            richTextBox_MF_out.Text += "U =   " + U;
+        }
+
+        private bool CheckRange()
+        {
+            try
+            {
+                if (Convert.ToDouble(textBox_X1.Text) < -1)
+                    return false;
+                if (Convert.ToDouble(textBox_X1.Text) > 1)
+                    return false;
+                if (Convert.ToDouble(textBox_X2.Text) < -1)
+                    return false;
+                if (Convert.ToDouble(textBox_X2.Text) > 1)
+                    return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
